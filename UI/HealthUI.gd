625cd335a8f1524c2553a56afd52a83326f04098
Eric Do <eric.do@go1.com>
2022-12-11 02:05:02 +1000

    add health ui

---
+++ UI/HealthUI.gd
@@ -0,0 +1,24 @@
+extends Control
+
+var max_hearts = 4 setget set_max_hearts
+var hearts = 4 setget set_hearts
+
+onready var heart_ui = $HeartUI
+onready var heart_ui_empty = $HeartUIEmpty
+
+func set_hearts(value):
+	hearts = clamp(value, 0, max_hearts)
+	if heart_ui != null:
+		heart_ui.rect_size.x = hearts * 15
+
+func set_max_hearts(value):
+	max_hearts = max(value, 1)
+	self.hearts = min(hearts, max_hearts)
+	if heart_ui_empty != null:
+		heart_ui_empty.rect_size.x = max_hearts * 15
+
+func _ready():
+	self.max_hearts = PlayerStats.max_health
+	self.hearts = PlayerStats.health
+	PlayerStats.connect("on_health_changed", self, "set_hearts")
+	PlayerStats.connect("on_max_health_changed", self, "set_max_hearts")
--- stats.gd
+++ stats.gd
@@ -1,10 +1,21 @@
 extends Node
 
-export var max_health = 1
-onready var health = max_health setget set_health
+export var max_health = 6 setget set_max_health
+var health = max_health setget set_health
 
 signal on_no_health
+signal on_health_changed(value)
+signal on_max_health_changed(value)
 
 func set_health(value):
 	health = value
+	emit_signal("on_health_changed", health)
 	if health <= 0: emit_signal("on_no_health")
+
+func set_max_health(value):
+	max_health = value
+	self.health = min(health, max_health)
+	emit_signal("on_max_health_changed", max_health)
+
+func _ready():
+	self.health = max_health
